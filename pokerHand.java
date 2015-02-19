0113c849f2bffca955b51ee0a778faf65539f20f
thomasloesch <tommy.loesch@gmail.com>
2015-02-19 18:34:43 -0500

    Implemented isMiniFlush() in pokerHand.java

--- pokerHand.java
+++ pokerHand.java
@@ -42,24 +42,28 @@ public class pokerHand extends Hand {
 	}
 	
 	public int isMiniFlush() {
-		// TODO - implement function without moving around the cards (create local copy of hand?)
-		sortBySuit();
-		
-		if(hand.get(0).getSuit() == hand.get(4).getSuit()) return -1; // check to see if the hand is a full flush
-		
-		int suit;
+		int suit, i;
 		int temp0 = hand.get(0).getSuit();
 		int temp1 = hand.get(1).getSuit();
-		// check to see if there are more than 2 different suits in the hand
-		for(int i = 2; i < 4; i++){
+		for(i = 2; i < 5; i++) {				// check to see if there are more than 2 different suits in the hand
 			suit = hand.get(i).getSuit();
-			if(suit != temp0){
+			if(suit != temp0) {
 				if(temp0 == temp1)	temp1 = suit;
 				else if(suit != temp1) return -1;
 			}
 		}
 		
+		if(temp0 == temp1) return -1;	// check to see if the hand is a full flush (there is only 1 suit)
+		
 		// NOTE - at this point there are 2 and only 2 different suits in the hand
+		suit = 0;
+		for(i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				if(hand.get(i).getSuit() == hand.get(j).getSuit()) suit++;
+			}
+			if(suit == 1) return i;    // if a card only finds 1 other card of the same suit (itself), return that position
+			suit = 0;
+		}
 		
 		return -1;
 	}
