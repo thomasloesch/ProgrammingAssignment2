86dd99af16b4c314d8fcc09b76e6df7941ed4e0f
thomasloesch <tommy.loesch@gmail.com>
2015-02-20 18:04:03 -0500

    Added additional comments
    Added more comments explaining isMiniFlush() and isMiniStraight().

--- pokerHand.java
+++ pokerHand.java
@@ -42,6 +42,7 @@ public class pokerHand extends Hand {
 	
 	public int isMiniFlush() {
 		int suit, i;
+		
 		int temp0 = hand.get(0).getSuit();
 		int temp1 = hand.get(1).getSuit();
 		for(i = 2; i < 5; i++) {				// check to see if there are more than 2 different suits in the hand
@@ -55,44 +56,46 @@ public class pokerHand extends Hand {
 		if(temp0 == temp1) return -1;	// check to see if the hand is a full flush (there is only 1 suit)
 		
 		// NOTE - at this point there are 2 and only 2 different suits in the hand
+		// this nested loop checks to see how many other cards of the same suit there are
+		// for every card in hand
 		suit = 0;
 		for(i = 0; i < 5; i++) {
 			for(int j = 0; j < 5; j++) {
 				if(hand.get(i).getSuit() == hand.get(j).getSuit()) suit++;
 			}
-			if(suit == 1) return i;    // if a card only finds 1 other card of the same suit (itself), return that position
+			if(suit == 1) return i;		// if a card only finds 1 other card of the same suit (itself), return that position
 			suit = 0;
 		}
 		
-		return -1;
+		return -1; 						// otherwise, there wasn't a mini flush
 	}
 	
 	public int isMiniStraight() {
-		Hand tempHand = new Hand();
-		int conflict = -1;
+		Hand tempHand = new Hand(); // create a temporary hand so we can reorder it without changing the actual hand
+		int conflict = -1;			// tracks the index of a conflict
 		int i;
 		
-		for(i = 0; i < this.getCardCount(); i++)
+		for(i = 0; i < this.getCardCount(); i++)	// fill tempHand with the cards in hand
 			tempHand.addCard(hand.get(i));
 		
 		tempHand.sortByValue();
 		
 		for(i = 1; i < 5; i++){
-			if( ( tempHand.getCard(i).getValue() != tempHand.getCard(i - 1).getValue() + 1 ) ){
-				if ( i == 2 || i == 3 )
+			if( ( tempHand.getCard(i).getValue() != tempHand.getCard(i - 1).getValue() + 1 ) ){	// check for a conflict
+				if ( i == 2 || i == 3 )									// if the conflict occurred in the middle, it isn't a mini flush
 					return -1;
-				else if( conflict == -1 )
+				else if( conflict == -1 )								// if there hasn't been a conflict yet, store the appropriate index in conflict
 					if(i == 1)
 						conflict = 0;
 					else
 						conflict = 4;
-				else return -1;
+				else return -1;											// otherwise, there has been more than 1 conflict, so it isn't a mini flush
 			}
 		}
 		
-		if(conflict > -1)
+		if(conflict > -1) 	
 			return hand.indexOf(tempHand.getCard(conflict));
-		else 
-			return -1;
+		else
+			return -1;	// if conflict didn't change, we know it was a flush, and therefore hand is not a mini flush
 	}
 }
--- tests.java
+++ tests.java
@@ -1,7 +1,7 @@
 
 public class tests {
 
-	public static void testIsMiniStraight(){
+	public static void testIsMiniStraight() {
 		pokerHand testHand = new pokerHand();
 		int retval;
 		
@@ -59,9 +59,68 @@ public class tests {
 			System.out.println( testHand.getCard(i) );
 		System.out.println( retval );
 	}
-
-	public static void runTests(){
-		testIsMiniStraight();
+	
+	public static void testIsMiniFlush() {
+		pokerHand testHand = new pokerHand();
+		int retval;
 		
+		testHand.addCard(new Card(3, Card.CLUBS));
+		testHand.addCard(new Card(4, Card.CLUBS));
+		testHand.addCard(new Card(5, Card.CLUBS));
+		testHand.addCard(new Card(6, Card.CLUBS));
+		testHand.addCard(new Card(7, Card.CLUBS));
+		retval = testHand.isMiniFlush();
+		for(int i = 0; i < 5; i++)
+			System.out.println( testHand.getCard(i) );
+		System.out.println( retval );
+		
+		testHand.clear();
+		testHand.addCard(new Card(3, Card.CLUBS));
+		testHand.addCard(new Card(4, Card.HEARTS));
+		testHand.addCard(new Card(5, Card.CLUBS));
+		testHand.addCard(new Card(6, Card.CLUBS));
+		testHand.addCard(new Card(7, Card.CLUBS));
+		retval = testHand.isMiniFlush();
+		for(int i = 0; i < 5; i++)
+			System.out.println( testHand.getCard(i) );
+		System.out.println( retval );
+		
+		testHand.clear();
+		testHand.addCard(new Card(3, Card.CLUBS));
+		testHand.addCard(new Card(4, Card.HEARTS));
+		testHand.addCard(new Card(5, Card.CLUBS));
+		testHand.addCard(new Card(6, Card.HEARTS));
+		testHand.addCard(new Card(7, Card.CLUBS));
+		retval = testHand.isMiniFlush();
+		for(int i = 0; i < 5; i++)
+			System.out.println( testHand.getCard(i) );
+		System.out.println( retval );
+		
+		testHand.clear();
+		testHand.addCard(new Card(3, Card.CLUBS));
+		testHand.addCard(new Card(4, Card.CLUBS));
+		testHand.addCard(new Card(5, Card.CLUBS));
+		testHand.addCard(new Card(6, Card.HEARTS));
+		testHand.addCard(new Card(7, Card.CLUBS));
+		retval = testHand.isMiniFlush();
+		for(int i = 0; i < 5; i++)
+			System.out.println( testHand.getCard(i) );
+		System.out.println( retval );
+		
+		testHand.clear();
+		testHand.addCard(new Card(3, Card.CLUBS));
+		testHand.addCard(new Card(4, Card.CLUBS));
+		testHand.addCard(new Card(5, Card.CLUBS));
+		testHand.addCard(new Card(6, Card.CLUBS));
+		testHand.addCard(new Card(7, Card.HEARTS));
+		retval = testHand.isMiniFlush();
+		for(int i = 0; i < 5; i++)
+			System.out.println( testHand.getCard(i) );
+		System.out.println( retval );
+	}
+
+	public static void runTests() {
+		//testIsMiniStraight();
+		testIsMiniFlush();
 	}
 }
