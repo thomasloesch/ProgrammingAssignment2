0df52075acc921d394b73339d6a3628a6b912903
thomasloesch <tommy.loesch@gmail.com>
2015-02-27 17:09:57 -0500

    Implemented taltalPlayer discard() no AI
    Also changed humPlayer to use TextIO.java and not Scanner

--- humPlayer.java
+++ humPlayer.java
@@ -1,6 +1,3 @@
-import java.util.Scanner;
-
-
 public class humPlayer extends pokerPlayer {
 
 	public humPlayer(int s, String N) {
@@ -12,10 +9,9 @@ public class humPlayer extends pokerPlayer {
 	public int discard(double handCode, Hand faceUpCards) {
 		int n = 0;
 		int responseAry[];
-		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("You may discard up to two cards.\nHow many cards would you like to discard?\t");
-		n = scan.nextInt();
+		System.out.print("You may discard up to two cards.\nHow many cards would you like to discard:\t");
+		n = TextIO.getInt();
 		
 		if(n > 0)
 			for (int i = 0; i < hand.getCardCount(); i++)
@@ -24,14 +20,18 @@ public class humPlayer extends pokerPlayer {
 		responseAry = new int[n];
 		
 		for(int i = 0; i < n; i++) {
-			System.out.print("Which of these cards would you like to discard?\t");
-			responseAry[i] = scan.nextInt();	
+			System.out.print("Which of these cards would you like to discard");
+			responseAry[i] = TextIO.getInt();
+		}
+		
+		if(responseAry.length == 2 && responseAry[0] > responseAry[1]) {
+			int temp = responseAry[0];
+			responseAry[0] = responseAry[1];
+			responseAry[1] = temp;
 		}
 		
 		for(int i = n - 1; i >= 0; i--)
 			hand.removeCard(responseAry[i]);
-		
-		scan.close();
 		
 		return n;
 	}
--- taltalPlayer.java
+++ taltalPlayer.java
@@ -8,8 +8,19 @@ public class taltalPlayer extends pokerPlayer {
 
 	@Override
 	public int discard(double handCode, Hand faceUpCards) {
-		// TODO Auto-generated method stub
-		return 0;
+		int temp;
+		
+		for(int i = 0; i < 2; i++) {
+			temp = 0;
+			
+			for(int j = 1; j < hand.getCardCount(); j++)
+				if(hand.getCard(j).getValue() > hand.getCard(temp).getValue()) temp = j;
+			
+			hand.removeCard(temp);
+		}
+			
+		
+		return 2;
 	}
 
 }
