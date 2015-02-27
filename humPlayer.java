d9edd6d3c3aefbddf5d54ea843868a897fdd1335
thomasloesch <tommy.loesch@gmail.com>
2015-02-27 17:07:29 -0500

    Implemented humPlayer discard()
    Small bug, there seem to be question marks added while waiting for every input other than the first input.

--- humPlayer.java
+++ humPlayer.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 public class humPlayer extends pokerPlayer {
 
@@ -8,8 +10,30 @@ public class humPlayer extends pokerPlayer {
 
 	@Override
 	public int discard(double handCode, Hand faceUpCards) {
-		// TODO Auto-generated method stub
-		return 0;
+		int n = 0;
+		int responseAry[];
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("You may discard up to two cards.\nHow many cards would you like to discard?\t");
+		n = scan.nextInt();
+		
+		if(n > 0)
+			for (int i = 0; i < hand.getCardCount(); i++)
+				System.out.println( i + ":\t" + hand.getCard(i).toString());
+			
+		responseAry = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print("Which of these cards would you like to discard?\t");
+			responseAry[i] = scan.nextInt();	
+		}
+		
+		for(int i = n - 1; i >= 0; i--)
+			hand.removeCard(responseAry[i]);
+		
+		scan.close();
+		
+		return n;
 	}
 
 }
